ff9c975460e92935677647e222063ce00b8361e8
MeepWagon <miniman460films@gmail.com>
2022-08-27 07:56:59 -0500

    second update?

--- src/client/scriptExecutor/init.client.lua
+++ src/client/scriptExecutor/init.client.lua
@@ -7,19 +7,21 @@ local plr = game.Players.LocalPlayer
 local Roact = require(repStor.Packages.roact)
 local RoactUI = require(script.RoactUI)
 
-local textBox = plr.PlayerGui:WaitForChild("RoactUI"):WaitForChild("Frame"):WaitForChild("TextBox")
+local textBox
 
 plr.CharacterAdded:Connect(function()
     Roact.mount(RoactUI, Players.LocalPlayer.PlayerGui, "RoactUI")
     workspace.Map["Piano Bundle"]:WaitForChild("PianoGui").Parent = plr.PlayerGui
-end)
 
-textBox.FocusLost:Connect(function(bool)
-	if bool then
-		remoteEvent:FireServer(textBox.Text, plr)
-		textBox.Text = "Sent"
-		task.wait(1.5)
-		textBox.Text = "Paste script here!"
-		textBox:ReleaseFocus()
-	end
+	textBox = plr.PlayerGui:WaitForChild("RoactUI"):WaitForChild("Frame"):WaitForChild("TextBox")
+	
+	textBox.FocusLost:Connect(function(bool)
+		if bool then
+			remoteEvent:FireServer(textBox.Text, plr)
+			textBox.Text = "Sent"
+			task.wait(1.5)
+			textBox.Text = "Paste script here!"
+			textBox:ReleaseFocus()
+		end
+	end)
 end)
